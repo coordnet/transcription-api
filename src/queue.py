9bfd67c7ac980793960ef65d12b9f2447f8782fa
Max Novakovic <max@lateral.io>
2024-11-04 18:24:38 +0100

    build: Update production service names

--- src/queue.py
+++ src/queue.py
@@ -4,6 +4,6 @@ import redis
 from rq import Queue
 
 
-redis_url = os.getenv("REDIS_URL", "redis://redis:6379")
+redis_url = os.getenv("REDIS_URL", "redis://coord_transcription_redis:6379")
 redis_connection = redis.from_url(redis_url)
 rq_queue = Queue(connection=redis_connection)
